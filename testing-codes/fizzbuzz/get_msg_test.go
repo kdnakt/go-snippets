6c13201b7559a79b1474b522110a06b70298c9e5
kdnakt <a.kid.1985@gmail.com>
2019-10-03 08:06:19 +0900

    rewrite with Subtest

--- testing-codes/fizzbuzz/get_msg_test.go
+++ testing-codes/fizzbuzz/get_msg_test.go
@@ -8,29 +8,36 @@ import (
 
 func TestGetMsg(t *testing.T) {
 	tests := []struct {
+		desc string
 		num  int
 		want string
 	}{
 		{
+			desc: "divisible by 15",
 			num:  15,
 			want: "FizzBuzz",
 		},
 		{
+			desc: "divisible by 5",
 			num:  5,
 			want: "Buzz",
 		},
 		{
+			desc: "divisible by 3",
 			num:  3,
 			want: "Fizz",
 		},
 		{
+			desc: "not divisible",
 			num:  4,
 			want: "4",
 		},
 	}
 	for _, tt := range tests {
-		if got := fizzbuzz.GetMsg(tt.num); got != tt.want {
-			t.Errorf("GetMsg(%d) = %s, want %s", tt.num, got, tt.want)
-		}
+		t.Run(tt.desc, func(t *testing.T) {
+			if got := fizzbuzz.GetMsg(tt.num); got != tt.want {
+				t.Errorf("GetMsg(%d) = %s, want %s", tt.num, got, tt.want)
+			}
+		})
 	}
 }
