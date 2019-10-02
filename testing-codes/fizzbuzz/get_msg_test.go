d9ebc6e6122e8132722eca627034f87213006d94
kdnakt <a.kid.1985@gmail.com>
2019-10-03 08:01:28 +0900

    use Table Driven test

--- testing-codes/fizzbuzz/get_msg_test.go
+++ testing-codes/fizzbuzz/get_msg_test.go
@@ -7,30 +7,30 @@ import (
 )
 
 func TestGetMsg(t *testing.T) {
-	var num int
-	var want string
-
-	num = 15
-	want = "FizzBuzz"
-	if got := fizzbuzz.GetMsg(num); got != want {
-		t.Errorf("GetMsg(%d) = %s, want %s", num, got, want)
+	tests := []struct {
+		num  int
+		want string
+	}{
+		{
+			num:  15,
+			want: "FizzBuzz",
+		},
+		{
+			num:  5,
+			want: "Buzz",
+		},
+		{
+			num:  3,
+			want: "Fizz",
+		},
+		{
+			num:  4,
+			want: "4",
+		},
 	}
-
-	num = 5
-	want = "Buzz"
-	if got := fizzbuzz.GetMsg(num); got != want {
-		t.Errorf("GetMsg(%d) = %s, want %s", num, got, want)
-	}
-
-	num = 3
-	want = "Fizz"
-	if got := fizzbuzz.GetMsg(num); got != want {
-		t.Errorf("GetMsg(%d) = %s, want %s", num, got, want)
-	}
-
-	num = 4
-	want = "4"
-	if got := fizzbuzz.GetMsg(num); got != want {
-		t.Errorf("GetMsg(%d) = %s, want %s", num, got, want)
+	for _, tt := range tests {
+		if got := fizzbuzz.GetMsg(tt.num); got != tt.want {
+			t.Errorf("GetMsg(%d) = %s, want %s", tt.num, got, tt.want)
+		}
 	}
 }
